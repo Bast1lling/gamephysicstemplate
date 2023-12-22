b24e2303521cc26f6225546ac5de40c961089b56
Sebastian Illing <sebastian.illing@tum.de>
2023-12-22 18:31:19 +0100

    ex3

--- Simulations/DiffusionSimulator.cpp
+++ Simulations/DiffusionSimulator.cpp
@@ -1,15 +1,21 @@
-#include "DiffusionSimulator.h"
+﻿#include "DiffusionSimulator.h"
 #include "pcgsolver.h"
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 
 DiffusionSimulator::DiffusionSimulator()
+	: alpha(0.03), xMin(-0.5), xMax(0.5), yMin(-0.5), yMax(0.5),
+	desiredHeight(128), desiredWidth(128), T(Grid(-0.5, 0.5, -0.5, 0.5, 128, 128))
 {
 	m_iTestCase = 0;
 	m_vfMovableObjectPos = Vec3();
 	m_vfMovableObjectFinalPos = Vec3();
 	m_vfRotate = Vec3();
-	// rest to be implemented
+	time = 0;
+	//rest to be implemented
+	//initSimpleDemo();
 }
 
 const char * DiffusionSimulator::getTestCasesStr(){
@@ -20,13 +26,17 @@ void DiffusionSimulator::reset(){
 		m_mouse.x = m_mouse.y = 0;
 		m_trackmouse.x = m_trackmouse.y = 0;
 		m_oldtrackmouse.x = m_oldtrackmouse.y = 0;
-
+		this->T = Grid(-0.5, 0.5, -0.5, 0.5, desiredHeight, desiredHeight);
 }
 
 void DiffusionSimulator::initUI(DrawingUtilitiesClass * DUC)
 {
 	this->DUC = DUC;
 	// to be implemented
+	TwAddVarRW(DUC->g_pTweakBar, "Alpha", TW_TYPE_FLOAT, &alpha, "min=0 step=0.01");
+	TwAddVarRW(DUC->g_pTweakBar, "Grid width", TW_TYPE_INT32, &desiredWidth, "min=2 max=128 step=1");
+	TwAddVarRW(DUC->g_pTweakBar, "Grid height", TW_TYPE_INT32, &desiredHeight, "min=2 max=128 step=1");
+	//TwAddVarCB(DUC->g_pTweakBar, "Grid width", TW_TYPE_INT32, DiffusionSimulator::WidthSetCallback, DiffusionSimulator::WidthGetCallback, this, "");
 }
 
 void DiffusionSimulator::notifyCaseChanged(int testCase)
@@ -34,6 +44,7 @@ void DiffusionSimulator::notifyCaseChanged(int testCase)
 	m_iTestCase = testCase;
 	m_vfMovableObjectPos = Vec3(0, 0, 0);
 	m_vfRotate = Vec3(0, 0, 0);
+	this->T = Grid(-0.5, 0.5, -0.5, 0.5, desiredWidth, desiredHeight);
 	//
 	// to be implemented
 	//
@@ -51,58 +62,134 @@ void DiffusionSimulator::notifyCaseChanged(int testCase)
 	}
 }
 
-void DiffusionSimulator::diffuseTemperatureExplicit() {
-// to be implemented
+void DiffusionSimulator::diffuseTemperatureExplicit(float timeElapsed) {
+	// to be implemented
+	int arraySize = desiredHeight*desiredWidth;
+	double* T_new = new double[arraySize];
+	for(int i = 0; i< desiredWidth; i++){
+		for(int j = 0; j< desiredHeight; j++){
+			double T_center = T.get(i,j);
+			double T_east = T.get(i+1,j);
+			double T_west = T.get(i-1,j);
+			double T_south = T.get(i,j+1);
+			double T_north = T.get(i,j-1);
+			double x = (T_west - 2*T_center + T_east)/(T.xDelta*T.xDelta);
+			double y = (T_north - 2*T_center + T_south)/(T.yDelta*T.yDelta);
+			double T_center_new = T_center + alpha*timeElapsed*(x+y);
+			T_new[i*desiredWidth + j] = T_center_new;
+		}
+	}
+
+	for (int i = 0; i < desiredWidth; i++) {
+		for (int j = 0; j < desiredHeight; j++) {
+			T.set(i,j,T_new[i*desiredWidth + j]);
+		}
+	}
 }
 
 
-void DiffusionSimulator::diffuseTemperatureImplicit() {
-	// solve A T = b
+void DiffusionSimulator::diffuseTemperatureImplicit(float timeElapsed) {
 
-	// This is just an example to show how to work with the PCG solver,
-	const int nx = 5;
-	const int ny = 5;
-	const int nz = 5;
-	const int N = nx * ny * nz;
+	double lambda = alpha*timeElapsed/(T.xDelta*T.xDelta);
+	const int N = T.width * T.height;
 
 	SparseMatrix<Real> A(N);
 	std::vector<Real> b(N);
+	std::vector<Real> x(N);
 
-	// This is the part where you have to assemble the system matrix A and the right-hand side b!
-
-	// perform solve
 	Real pcg_target_residual = 1e-05;
 	Real pcg_max_iterations = 1000;
 	Real ret_pcg_residual = 1e10;
 	int  ret_pcg_iterations = -1;
-
 	SparsePCGSolver<Real> solver;
 	solver.set_solver_parameters(pcg_target_residual, pcg_max_iterations, 0.97, 0.25);
 
-	std::vector<Real> x(N);
-	for (int j = 0; j < N; ++j) { x[j] = 0.; }
-
-	// preconditioners: 0 off, 1 diagonal, 2 incomplete cholesky
+	//for every pixel fill A, b and x
+	for (int i = 0; i < N; i++) {
+		x[i] = 0;
+		b[i] = T.T[i];
+		//set diagonal
+		A.set_element(i, i, 1 + 4*lambda);
+		//set neighbors
+		//if we are not on the left border
+		if (i % T.width != 0) {
+			A.set_element(i, i - 1, -lambda);
+		}
+		//if we are not on the right border
+		if (i % T.width != T.width - 1) {
+			A.set_element(i, i + 1, -lambda);
+		}
+		//if we are not on the top border
+		if (i >= T.width) {
+			A.set_element(i, i - T.width, -lambda);
+		}
+		//if we are not on the bottom border
+		if (i < N - T.width) {
+			A.set_element(i, i + T.width, -lambda);
+		}
+	}
+	/*//iterate over all entrie of A
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			std::cout << A(i, j) << " ";
+		}
+		std::cout << " = " << b[i] << "\n";
+	}
+	*/
 	solver.solve(A, b, x, ret_pcg_residual, ret_pcg_iterations, 0);
+	T.setAll(x);
+}
 
-	// Final step is to extract the grid temperatures from the solution vector x
-	// to be implemented
+void Grid::printVector(double* b)
+{
+	//print contents of b
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++) {
+			std::cout << b[i*width + j] << " ";
+		}
+		std::cout << "\n";
+	}
+}
+
+void DiffusionSimulator::printMatrix(double* A)
+{
+}
+
+void DiffusionSimulator::createGrid(Grid old_grid)
+{	delete[] T.T;
+	T = Grid(-0.5, 0.5, -0.5, 0.5, desiredWidth, desiredHeight);
+	//for every pixel
+	for (int i = 0; i < desiredWidth; i++) {
+		for (int j = 0; j < desiredHeight; j++) {
+			//if we are in the old grid
+			if (i < old_grid.width && j < old_grid.height) {
+				T.set(i,j,old_grid.get(i,j));
+			}
+			else {
+				T.set(i,j,0);
+			}
+		}
+	}
 }
 
 
 
 void DiffusionSimulator::simulateTimestep(float timeStep)
 {
+	if (desiredWidth != T.width || desiredHeight != T.height) {
+		//T.adjustGrid(desiredWidth, desiredHeight);
+		createGrid(T);
+	}
 	// update current setup for each frame
 	switch (m_iTestCase)
 	{
 	case 0:
 		// feel free to change the signature of this function
-		diffuseTemperatureExplicit();
+		diffuseTemperatureExplicit(timeStep);
 		break;
 	case 1:
 		// feel free to change the signature of this function
-		diffuseTemperatureImplicit();
+		diffuseTemperatureImplicit(timeStep);
 		break;
 	}
 }
@@ -111,6 +198,22 @@ void DiffusionSimulator::drawObjects()
 {
 	// to be implemented
 	//visualization
+	//draw the grid
+	for(int i = 0; i< desiredWidth; i++){
+		for(int j = 0; j< desiredHeight; j++){
+			Vec3 pos = Vec3(xMin + i*T.xDelta, yMin + j*T.yDelta, 0);
+			double size = 0.01;
+			double heat = T.get(i,j);
+			Vec3 color = Vec3();
+			if(heat > 0)
+				color = Vec3(heat, heat, heat);
+			else
+				color = Vec3(heat,0,0);
+
+			DUC->setUpLighting(Vec3(), 0.4*Vec3(1,1,1), 100, 0.6*color);
+			DUC->drawSphere(pos, Vec3(T.xDelta/2, T.yDelta/2, size));
+		}
+	}
 }
 
 
@@ -131,4 +234,78 @@ void DiffusionSimulator::onMouse(int x, int y)
 	m_oldtrackmouse.y = y;
 	m_trackmouse.x = x;
 	m_trackmouse.y = y;
+}
+
+Grid::Grid(double xMin, double xMax, double yMin, double yMax, int width, int height): xMin(xMin), yMin(yMin), xMax(xMax), yMax(yMax), width(width), height(height)
+{
+	xDelta = (xMax - xMin)/width;
+	yDelta = (yMax - yMin)/height;
+	T = new double[width*height];
+	std::srand(std::time(0));
+	//X
+	for (int i = 0; i < width; i++) {
+		//Y
+		for (int j = 0; j < height; j++) {
+			T[i*width + j] = f_0(xMin + i*xDelta, yMin + j*yDelta);
+		}
+	}
+}
+
+double Grid::get(int i, int j)
+{
+	if(i < 0 || i >= width || j < 0 || j >= height)
+		return 0;
+	//printVector(T);
+	return T[i*width + j];
+}
+
+void Grid::set(int i, int j, double val)
+{
+	if(i < 0 || i >= width || j < 0 || j >= height)
+		return;
+
+	T[i*width + j] = val;
+}
+
+void Grid::setAll(std::vector<double> x)
+{
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++) {
+			T[i*width + j] = x[i*width + j];
+		}
+	}
+}
+
+void Grid::adjustGrid(int new_width, int new_height)
+{
+	width = new_width;
+	height = new_height;
+	xDelta = (xMax - xMin) / width;
+	yDelta = (yMax - yMin) / height;
+	/*
+	//X
+	for (int i = 0; i < width; i++) {
+		//Y
+		for (int j = 0; j < height; j++) {
+			T[i * width + j] = f_0(xMin + i * xDelta, yMin + j * yDelta);
+		}
+	}
+	*/
+}
+
+double Grid::f_0(double i, double j)
+{
+	//f0(x) = sin (πx/L)
+	double maxX = xMin + width*xDelta;
+	double maxY = yMin + height*yDelta;
+	double s = sin(M_PI*i/maxX);
+	double c = cos(M_PI*j/maxY);
+	//return 2*(s+c) - 1;
+	/*return sin(M_PI * i / maxX);
+	std::mt19937 eng;
+	std::uniform_real_distribution<float> randCol(-1.0f, 1.0f);
+	return randCol(eng);  */ 
+
+	return (static_cast<double>(std::rand()) / RAND_MAX) * 2 - 1;
+
 }
--- Simulations/DiffusionSimulator.h
+++ Simulations/DiffusionSimulator.h
@@ -1,4 +1,4 @@
-#ifndef DIFFUSIONSIMULATOR_h
+﻿#ifndef DIFFUSIONSIMULATOR_h
 #define DIFFUSIONSIMULATOR_h
 
 #include "Simulator.h"
@@ -6,6 +6,28 @@
 
 class Grid {
 	// to be implemented
+	public:
+		Grid(double xMin, double xMax, double yMin, double yMax, int width, int height);
+		double get(int i, int j);
+		void set(int i, int j, double val);
+		void setAll(std::vector<double> x);
+
+		void adjustGrid(int new_width, int new_height);
+
+		int width;
+		int height;
+		double xDelta;
+		double yDelta;
+		double *T;
+
+	private:
+		double f_0(double i, double j);
+		void printVector(double* b);
+
+		double xMin;
+		double yMin;
+		double xMax;
+		double yMax;
 };
 
 
@@ -29,10 +51,30 @@ public:
 	void drawObjects();
 
 	// Feel free to change the signature of these functions, add arguments, etc.
-	void diffuseTemperatureExplicit();
-	void diffuseTemperatureImplicit();
+	void diffuseTemperatureExplicit(float timeElapsed);
+	void diffuseTemperatureImplicit(float timeElapsed);
+
+	int desiredWidth;
+	int desiredHeight;
+
+	/*
+	static void TW_CALL WidthSetCallback(const void* value, void* clientData) {
+		DiffusionSimulator* sim = static_cast<DiffusionSimulator*>(clientData);
+		int new_width = *static_cast<const int*>(value);
+		sim->T = Grid(-0.5, 0.5, -0.5, 0.5, new_width, sim->T.height);
+	};
+	static void TW_CALL WidthGetCallback(void* value, void* clientData) {
+		DiffusionSimulator* sim = static_cast<DiffusionSimulator*>(clientData);
+		*static_cast<int*>(value) = sim->T.width;
+	};
+	*/
 
 private:
+
+	// Functions	
+	void printMatrix(double* A);
+	void createGrid(Grid old_grid);
+
 	// Attributes
 	Vec3  m_vfMovableObjectPos;
 	Vec3  m_vfMovableObjectFinalPos;
@@ -41,6 +83,12 @@ private:
 	Point2D m_trackmouse;
 	Point2D m_oldtrackmouse;
 	Grid T;
+	double alpha;
+	double xMin;
+	double xMax;
+	double yMin;
+	double yMax;
+	double time;
 };
 
 #endif
